85ec21240ff64b1e3008d88bd0df4b8386756494
Matheos Profirescu <matheosprofirescu@yahoo.com>
2023-11-04 13:23:48 +0200

    sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,26 @@
+import Sidebar from '../sidebar/Sidebar'
 import './navbar.scss'
+import { motion } from 'framer-motion'
 
 export const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <div className="wrapper">
-            <span>Profirescu Matheos</span>
-            <div className="social">
-                <a href=""><img src="/facebook.png" alt="" /></a>
-                <a href=""><img src="/instagram.png" alt="" /></a>
-                <a href=""><img src="/youtube.png" alt="" /></a>
-                <a href=""><img src="/dribbble.png" alt="" /></a>
+    return (
+        <div className='navbar'>
+            <Sidebar />
+            <div className="wrapper">
+                <motion.span
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                >
+                    Profirescu Matheos
+                </motion.span>
+                <div className="social">
+                    <a href=""><img src="/facebook.png" alt="" /></a>
+                    <a href=""><img src="/instagram.png" alt="" /></a>
+                    <a href=""><img src="/youtube.png" alt="" /></a>
+                    <a href=""><img src="/dribbble.png" alt="" /></a>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
