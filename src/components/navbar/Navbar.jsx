c45c5252be93479c7a6c4a783bd58249d9dd9710
Matheos Profirescu <matheosprofirescu@yahoo.com>
2023-11-04 19:08:03 +0200

    hero page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,37 @@
 import Sidebar from '../sidebar/Sidebar'
 import './navbar.scss'
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
-export const Navbar = () => {
+// ... Other imports
+
+const Navbar = () => {
+    // variants for the container
+    const containerVariants = {
+        hidden: {
+            transition: {
+                staggerChildren: 0.1,
+                staggerDirection: -1
+            }
+        },
+        visible: {
+            transition: {
+                staggerChildren: 0.3,
+            }
+        }
+    };
+
+    // variants for individual items
+    const itemVariants = {
+        hidden: {
+            opacity: 0,
+            scale: 0.5
+        },
+        visible: {
+            opacity: 1,
+            scale: 1
+        }
+    };
+
     return (
         <div className='navbar'>
             <Sidebar />
@@ -14,13 +43,16 @@ export const Navbar = () => {
                 >
                     Profirescu Matheos
                 </motion.span>
-                <div className="social">
-                    <a href=""><img src="/facebook.png" alt="" /></a>
-                    <a href=""><img src="/instagram.png" alt="" /></a>
-                    <a href=""><img src="/youtube.png" alt="" /></a>
-                    <a href=""><img src="/dribbble.png" alt="" /></a>
-                </div>
+                <motion.div className="social" variants={containerVariants} initial="hidden" animate="visible">
+                    {['/facebook.png', '/instagram.png', '/youtube.png', '/dribbble.png'].map((icon, index) => (
+                        <motion.a key={index} variants={itemVariants}>
+                            <img src={icon} alt="" />
+                        </motion.a>
+                    ))}
+                </motion.div>
             </div>
         </div>
     )
 }
+
+export default Navbar
